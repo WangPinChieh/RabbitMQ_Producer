dc0b40659914c6a253c41faf2687d00ad66a32ec
Jay Wang <jay.wang@hp.com>
2022-01-04 17:40:55 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() {HostName = "localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("hello", false, false, false, null);
+
+while (true)
+{
+    var message = Console.ReadLine();
+    channel.BasicPublish("", "hello", null, Encoding.UTF8.GetBytes(message));
+    Console.WriteLine($"Message Sent: {message}");
+}
+
+Console.ReadLine();
